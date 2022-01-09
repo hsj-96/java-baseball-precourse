d13994dcf800381b4e19986f695541f9db27dd79
1101706 <a1101706@1101706ui-MacBookPro.local>
2022-01-09 23:54:43 +0900

    feat : 숫자 중복 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import java.util.Arrays;
+import nextstep.utils.*;
+
 public class Application {
+    // # 숫자 중복 체크
+    boolean isDuplicated(int[] numbers) {
+        int freqCounter[] = new int[9];
+        Arrays.fill(freqCounter, 0);
+
+        for (int i = 0; i < 3; i++) {
+            int number = numbers[i];
+            if (freqCounter[number - 1] > 0) {
+                return true;
+            }
+
+            freqCounter[number - 1]++;
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
     }
 }
