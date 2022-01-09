7cda91f83992efb42a155ddeb35cc06b85130930
1101706 <a1101706@1101706ui-MacBookPro.local>
2022-01-09 23:55:39 +0900

    feat : 3자리의 중복없는 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,21 @@ import java.util.Arrays;
 import nextstep.utils.*;
 
 public class Application {
+    int computer[] = {0, 0, 0};
+    int user[] = {0, 0, 0};
+
+    // # 3자리의 중복없는 난수 생성
+    void setComputerRandomNumber() {
+        for (int i = 0; i < 3; i++) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            computer[i] = number;
+        }
+
+        if (isDuplicated(computer)) {
+            setComputerRandomNumber();
+        }
+    }
+
     // # 숫자 중복 체크
     boolean isDuplicated(int[] numbers) {
         int freqCounter[] = new int[9];
